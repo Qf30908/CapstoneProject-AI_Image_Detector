abcafa282d7b828140032e3dfc2147e15d645076
Qf30908 <147955058+Qf30908@users.noreply.github.com>
2026-05-31 18:09:58 +0200

    Frontend

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact() {
+    return (
+      <div className="page">
+        <div className="card">
+          <h1>Contact</h1>
+          <p>Project by: Qemal Fejzullai</p>
+          <p>Topic: AI-Based Image Authenticity Detector</p>
+          <p>University: South East European University</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Contact;
